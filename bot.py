21b636b48636628cefed73dc5445361612e2a3e3
RoninEMH <60003921+RoninEMH@users.noreply.github.com>
2020-11-10 21:27:34 +0200

    changing activity

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="You"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the voice channel"))
 
 
 @bot.command()
